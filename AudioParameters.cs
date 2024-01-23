b573dcc00d1c840906ff9eb0563575a78e9ac378
Cremetoertchen0815 <Mircosoft_heck@web.de>
2024-01-23 16:20:57 +0100

    Added machine learning

---
+++ AudioParameters.cs
@@ -0,0 +1,63 @@
+﻿namespace NeuronalesNetz;
+
+public struct AudioParameters
+{
+    public double Energy;
+    public double Rhythm;
+    public double Pitch;
+
+    public static AudioParameters FromSample(float[] sample) => new AudioParameters() { Energy = CalculateEnergy(sample), Rhythm = CalculateRhythmScore(sample), Pitch = CalculatePitchScore(sample) };
+
+    private static double CalculateEnergy(float[] buffer)
+    {
+        double energy = 0.0;
+
+        for (int i = 0; i < buffer.Length; i++)
+        {
+            energy += buffer[i] * buffer[i];
+        }
+        
+        return energy;
+    }
+
+    private static double CalculateRhythmScore(float[] buffer)
+    {
+        double rhythmScore = 0.0;
+
+        // Annahme: buffer enthält Gleitkommazahlenrepräsentation des Audiosignals
+
+        for (int i = 1; i < buffer.Length - 1; i++)
+        {
+            // Berechne die Differenzen zwischen aufeinanderfolgenden Abtastwerten
+            double diff1 = buffer[i] - buffer[i - 1];
+            double diff2 = buffer[i + 1] - buffer[i];
+            
+            // Wenn das aktuelle Sample größer ist als die umgebenden Samples,
+            // und somit ein lokales Maximum darstellt, erhöhe den Rhythmus-Score
+            if (diff1 > 0 && diff2 < 0)
+            {
+                rhythmScore += buffer[i] * buffer[i];
+            }
+        }
+
+        return rhythmScore;
+    }
+
+    private static double CalculatePitchScore(float[] buffer)
+    {
+        double pitchScore = 0.0;
+
+        // Annahme: buffer enthält Gleitkommazahlenrepräsentation des Audiosignals
+
+        for (int i = 1; i < buffer.Length; i++)
+        {
+            // Berechne die Änderung der Amplitude zwischen aufeinanderfolgenden Abtastwerten
+            double amplitudeChange = Math.Abs(buffer[i] - buffer[i - 1]);
+
+            // Addiere die Änderung zur Gesamtsumme
+            pitchScore += amplitudeChange;
+        }
+
+        return pitchScore;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,19 +4,38 @@ using NeuronalesNetz;
 
 var tr = new TraingDataGenerator();
 tr.LoadDataFromFolder(Path.GetFullPath("songs/"), AudioType.MUSIC);
-tr.SaveTestFile();
-Console.WriteLine();
+tr.LoadDataFromFolder(Path.GetFullPath("speech/"), AudioType.SPEECH);
+var trainingData = tr.GetTrainingSamples(5000).Select(item => (AudioParameters.FromSample(item.data), item.type)).ToArray();
+
+var guesses = 0;
+var correctGuesses = 0;
+
+foreach (var item in trainingData)
+{
+    var sampleData = new AudioModel.ModelInput()
+    {
+        Energy = (float)item.Item1.Energy,
+        Rhythm = (float)item.Item1.Rhythm,
+        Pitch = (float)item.Item1.Pitch,
+    };
+
+    //Load model and predict output
+    var result = AudioModel.Predict(sampleData);
+    var correctGuess = (AudioType)result.Type == item.type;
+    Console.WriteLine("Guess " + guesses);
+    if (correctGuess) correctGuesses++;
+    guesses++;
+}
+
+Console.WriteLine($"Guessed {correctGuesses} of {guesses} correct ({(float)correctGuesses / guesses * 100}%)");
 
 
+//var file = new StreamWriter("training_data.csv");
 
+//file.WriteLine("Energy,Rhythm,Pitch,Type");
+//foreach (var item in trainingData)
+//{
+//    file.WriteLine($"{item.Item1.Energy.ToString(System.Globalization.CultureInfo.InvariantCulture)},{item.Item1.Rhythm.ToString(System.Globalization.CultureInfo.InvariantCulture)},{item.Item1.Pitch.ToString(System.Globalization.CultureInfo.InvariantCulture)},{item.type}");
+//}
 
-
-
-//using CSharpNet;
-
-//var layers = new int[] { 3, 6, 3, 1 };
-//var learningRate = 0.1;
-//var activationFunction = ActivationFunction.Sigmoid;
-//var nn = new DeepNetBuilder(layers, learningRate, activationFunction);
-
-
+//file.Close();
